a2cb5d643fb6dfab891b945558901b2b75740e69
AeKanemoto <178535954+kanemotoae@users.noreply.github.com>
2024-11-14 11:51:36 +0900

    １１５１

--- src/main/java/com/example/demo3/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo3/config/WebSecurityConfig.java
@@ -12,9 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.demo3.service.AccountUserDetailsService;
 
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration // 設定を行うクラスであることを指定
+@EnableWebSecurity // Spring Securityを利用することを指定
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 追記 メソッド認可処理を有効化
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AccountUserDetailsService userDetailsService;
@@ -34,9 +34,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// 認可の設定
-		http.exceptionHandling().accessDeniedPage("/accessDeniedPage").and().authorizeRequests()
-				.antMatchers("/loginForm").permitAll() // loginFormは、全ユーザからのアクセスを許可
-				.anyRequest().authenticated(); // loginForm以外は、認証を求める
+		http.exceptionHandling() // 追記
+				.accessDeniedPage("/accessDeniedPage") // 追記 アクセス拒否された時に遷移するパス
+				.and() // 追記
+				.authorizeRequests().antMatchers("/loginForm").permitAll().anyRequest().authenticated(); // loginForm以外は、認証を求める
 
 		// ログイン設定
 		http.formLogin() // フォーム認証の有効化
@@ -44,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginProcessingUrl("/authenticate") // フォーム認証処理のパス
 				.usernameParameter("userName") // ユーザ名のリクエストパラメータ名
 				.passwordParameter("password") // パスワードのリクエストパラメータ名
-				.defaultSuccessUrl("/posts") // ※ ここを変更
+				.defaultSuccessUrl("/posts") // 認証成功時に遷移するデフォルトのパス
 				.failureUrl("/loginForm?error=true"); // 認証失敗時に遷移するパス
 
 		// ログアウト設定
